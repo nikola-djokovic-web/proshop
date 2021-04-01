1ff18bfecccea8c0d3975f01b70ef177c0ecbf84
nikola-djokovic-web <djokovicnikola69@gmail.com>
2021-04-01 23:23:41 +0200

    created order delivered button

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -15,7 +15,11 @@ import {
   ORDER_LIST_MY_RESET,
   ORDER_LIST_REQUEST,
   ORDER_LIST_SUCCESS,
-  ORDER_LIST_RESET,
+  ORDER_LIST_FAIL,
+  ORDER_DELIVER_REQUEST,
+  ORDER_DELIVER_SUCCESS,
+  ORDER_DELIVER_FAIL,
+  ORDER_DELIVER_RESET,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -95,9 +99,26 @@ export const orderListReducer = (state = { orders: [] }, action) => {
       };
     case ORDER_LIST_SUCCESS:
       return { loading: false, orders: action.payload };
-    case ORDER_LIST_MY_FAIL:
+    case ORDER_LIST_FAIL:
       return { loading: false, error: action.payload };
 
+    default:
+      return state;
+  }
+};
+
+export const orderDeliverReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ORDER_DELIVER_REQUEST:
+      return {
+        loading: true,
+      };
+    case ORDER_DELIVER_SUCCESS:
+      return { loading: false, success: true };
+    case ORDER_DELIVER_FAIL:
+      return { loading: false, error: action.DELIVERload };
+    case ORDER_DELIVER_RESET:
+      return {};
     default:
       return state;
   }
